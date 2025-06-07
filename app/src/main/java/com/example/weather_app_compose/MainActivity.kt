7ca10eecb4df537eeeda9e45a7234865a234eced
HendSayed25 <hendsayed853@gmail.com>
2025-06-07 13:38:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weather_app_compose/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.weather_app_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.weather_app_compose.ui.theme.Weather_app_composeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Weather_app_composeTheme {
+
+            }
+        }
+    }
+}
